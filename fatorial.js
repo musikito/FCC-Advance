b01d4d837230321f80448b2d11e3e5b97588c9fd
jose marte <tumusicalatina@gmail.com>
2019-05-21 09:56:46 -0400

    added factorial and longest word

---
+++ fatorial.js
@@ -0,0 +1,13 @@
+/**
+ * Return the factorial of the provided integer.
+ * @param {*} num 
+ */
+function factorialize(num) {
+    var factorial = 1;
+     for(var i = factorial; num > i;num--) {
+       factorial = num*factorial;
+     }
+     return factorial;
+   }
+   
+   factorialize(5);
