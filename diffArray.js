5876c193bed29dbcb7043ce7e9c231bd9c0fffaa
jose marte <tumusicalatina@gmail.com>
2019-06-13 13:07:27 -0400

    Added a new diff array with callback and filter

--- diffArray.js
+++ diffArray.js
@@ -1,13 +1,13 @@
 function diffArray(arr1, arr2) {
- var arr1Filtered = arr1.filter(function (value){
- return arr2.indexOf(value) === -1;
- });
+  var arr1Filtered = arr1.filter(function(value) {
+    return arr2.indexOf(value) === -1;
+  });
 
- var arr2Filtered = arr2.filter(function (value){
- return arr1.indexOf(value) === -1;
- });
+  var arr2Filtered = arr2.filter(function(value) {
+    return arr1.indexOf(value) === -1;
+  });
 
- return arr1Filtered.concat(arr2Filtered);
+  return arr1Filtered.concat(arr2Filtered);
 }
 
-diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
+console.log(diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]));
---
+++ diffArrayCallBack.js
@@ -0,0 +1,13 @@
+function diff(arr1, arr2) {
+  //merge both arrays to make it easy to compare
+  var newArr = arr1.concat(arr2);
+
+  function check(item) {
+    if (arr1.indexOf(item) === -1 || arr2.indexOf(item) === -1) {
+      return item;
+    }
+  }
+  //use filter to get the new array
+  return newArr.filter(check);
+}
+console.log(diff([1, 2, 3, 5], [1, 2, 3, 4, 5]));
